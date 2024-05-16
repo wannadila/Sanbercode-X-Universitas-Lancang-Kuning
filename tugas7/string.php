71797e5b29688f6a215f18e19b6f148a5108da88
wannadila <wannadila22@gmail.com>
2024-05-17 00:12:21 +0700

    commit pertama : menambah string

---
+++ tugas7/string.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String PHP</title>
+</head>
+<body>
+    <h1>Berlatih String PHP</h1>
+    <?php   
+        echo "<h3> Soal No 1</h3>";
+        /* 
+            SOAL NO 1
+            Tunjukkan dengan menggunakan echo berapa panjang dari string yang diberikan berikut! Tunjukkan juga jumlah kata di dalam kalimat tersebut! 
+
+            Contoh: 
+            $string = "PHP is never old";
+            Output:
+            Panjang string: 16, 
+            Jumlah kata: 4 
+        */
+
+        $first_sentence = "Hello PHP!" ; 
+        $second_sentence = "I'm ready for the challenges"; 
+
+        
+        echo "Kalimat: \"$first_sentence\"<br>";
+        echo "Panjang string: " . strlen($first_sentence) . "<br>";
+        echo "Jumlah kata: " . str_word_count($first_sentence) . "<br><br>";
+
+    
+        echo "Kalimat: \"$second_sentence\"<br>";
+        echo "Panjang string: " . strlen($second_sentence) . "<br>";
+        echo "Jumlah kata: " . str_word_count($second_sentence) . "<br>";
+
+        echo "<h3> Soal No 2</h3>";
+        /* 
+            SOAL NO 2
+            Mengambil kata pada string dan karakter-karakter yang ada di dalamnya. 
+        */
+        $string2 = "I love PHP";
+        
+        echo "<label>String: </label> \"$string2\" <br>";
+        $words = explode(' ', $string2);
+        echo "Kata pertama: " . $words[0] . "<br>"; 
+        echo "Kata kedua: " . $words[1] . "<br>";
+        echo "Kata ketiga: " . $words[2] . "<br>";
+
+        echo "<h3> Soal No 3 </h3>";
+        /*
+            SOAL NO 3
+            Mengubah karakter atau kata yang ada di dalam sebuah string.
+        */
+        $string3 = "PHP is old but sexy!";
+        $string3_replaced = str_replace("sexy", "awesome", $string3);
+        echo "String: \"$string3\"<br>"; 
+        echo "Hasil perubahan: \"$string3_replaced\"";
+
+    ?>
+</body>
+</html>
