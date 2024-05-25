53b132293bad6fa53032be4d22464cef30c1708d
wannadila <wannadila22@gmail.com>
2024-05-26 00:53:10 +0700

    tugas 11 laravel

---
+++ tugas_11_laravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController ::class,'home']);
+Route::get('/register', [AuthController::class,'register']);
+Route::get('/welcome', [AuthController::class,'welcome']);
