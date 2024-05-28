7f3c58f0430700a070a2f0c0868f33f5ffb6315f
wannadila <wannadila22@gmail.com>
2024-05-28 23:06:28 +0700

    tugas 12 wan

---
+++ tugas12/resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('title')
+    SanberBook
+@endsection
+@section('content')
+<p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+
+<h3>Benefit Join di SanberBook</h3>
+        <ul>
+            <li>Mendapatkan motivasi dari sesama developer</li>
+            <li>Sharing knowledge dari para mastah Sanber</li>
+            <li>Dibuat oleh calon web developer terbaik</li>
+        </ul>  
+
+<h3> Cara Bergabung ke SanberBook </h3>
+
+<ol>
+    <li>Mengunjungi website ini</li>
+    <li>Mendaftar <a href="/register">di Form Sign Up</a></li>
+    <li>Selesai!</li>
+@endsection
---
+++ tugas12/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+Selamat Datang     
+@endsection
+
+@section('content')
+<h1>SELAMAT DATANG <?php echo $fname; ?> <?php echo $lname; ?></h1>
+<h3> terima kasih telah bergabung di SanberBook . social media kita bersama </h3>
+@endsection
